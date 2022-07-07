310cd21a2826bc72d16ba5139b67ff51fd3dc68e
Ramon Duque <ramonduke@gmail.com>
2022-07-06 23:12:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = "Ramon";
+let lastName = "Duque";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const population = 672351;
+console.log(population);
+
+let _multpopulation = population * 3;
+console.log(_multpopulation);
+
+const javaScriptIsCool = true;
+const colors = ["red","blue","yellow","green","orange"]
+
+for (let i = 1; i <= 5; i++){
+    console.log(i);
+}
+
+for (let e = 0; e <= 4; e++){
+    console.log(e);
+}
